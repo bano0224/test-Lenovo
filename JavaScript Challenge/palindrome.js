328ec5eb3eb29ac1e35f730e55c9d078ea9eee6b
Bano <banorodriguez0224@gmail.com>
2021-10-07 15:36:26 -0300

    last commit

--- JavaScript Challenge/palindrome.js
+++ JavaScript Challenge/palindrome.js
@@ -3,5 +3,5 @@
 function palindrome(string) {
     const stringReverse = string.split("").reverse().join("")
   
-    return stringReverse === string ? "es palindromo" : "no es palindromo"
+    return stringReverse === string ? true : false
   }
